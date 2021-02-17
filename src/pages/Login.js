594694b0cd26348050b4d7d61684560feae6a2b3
oscles <eugenio.pajaro.ext@nokia.com>
2021-02-17 12:10:21 -0500

    --amend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,21 @@
-import { LoginUserForm } from "../components/LoginUserForm";
+import { LoginUserForm } from '../components/LoginUserForm';
 
 export function Login() {
   return (
-    <div className="bg-gradient-primary">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-9 col-lg-12 col-xl-10">
-            <div className="card shadow-lg o-hidden border-0 my-5">
-              <div className="card-body p-0">
-                <div className="row">
-                  <div className="col-lg-6 d-none d-lg-flex">
-                    <div className="flex-grow-1 bg-login-image">
-                      <img src=" ./assets/img/dogs/image3.jpeg" alt="" width="407px"/>
+    <div className='login bg-gradient-primary'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-9 col-lg-12 col-xl-10'>
+            <div className='card shadow-lg o-hidden border-0 my-5'>
+              <div className='card-body p-0'>
+                <div className='row'>
+                  <div className='col-lg-6 d-none d-lg-flex'>
+                    <div className='flex-grow-1 bg-login-image'>
+                      <img
+                        src=' ./assets/img/dogs/image3.jpeg'
+                        alt=''
+                        width='407px'
+                      />
                     </div>
                   </div>
                   <LoginUserForm />
--- src/pages/SingUp.js
+++ src/pages/SingUp.js
@@ -1,17 +1,21 @@
-import { RegisterUserForm } from "../components/RegisterUserForm";
+import { RegisterUserForm } from '../components/RegisterUserForm';
 
 export function SingUp() {
   return (
-    <div className="bg-gradient-primary">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-9 col-lg-12 col-xl-10">
-            <div className="card shadow-lg o-hidden border-0 my-5">
-              <div className="card-body p-0">
-                <div className="row">
-                  <div className="col-lg-6 d-none d-lg-flex">
-                    <div className="flex-grow-1 bg-login-image">
-                      <img src=" ./assets/img/dogs/image2.jpeg" alt="" width="407px"/>
+    <div className='registrarse bg-gradient-primary'>
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-md-9 col-lg-12 col-xl-10'>
+            <div className='card shadow-lg o-hidden border-0 my-5'>
+              <div className='card-body p-0'>
+                <div className='row'>
+                  <div className='col-lg-6 d-none d-lg-flex'>
+                    <div className='flex-grow-1 bg-login-image'>
+                      <img
+                        src=' ./assets/img/dogs/image2.jpeg'
+                        alt=''
+                        width='407px'
+                      />
                     </div>
                   </div>
                   <RegisterUserForm />
